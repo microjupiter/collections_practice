9bb0a853a7f4003393c8604c65ceab33e0d5a8f2
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-11 18:01:07 -0600

    commit tree 691b5a86361fec8cc1fdfd3faa9df10565e4ac2c

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(num)
+    num.sort
+end
 
+def sort_array_desc(num)
+    num.sort.reverse
+end
+
+def sort_array_char_count(num)
+    num.sort do |left, right|
+        left.length <=> right.length
+    end
+end
+
+def swap_elements(num)
+    num[1], num[2] = num[2], num[1]
+    num
+end
+
+def reverse_array(num)
+    num.reverse
+end
+
+def kesha_maker(num)
+    num.each do |item|
+        item[2] = "$"
+    end
+end
+
+def find_a(num)
+    num.find_all do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(num)
+    num.sum
+end
+
+def add_s(num)
+    num.collect do |word|
+        if word == "feet"
+            word
+        else
+            word + "s"
+        end
+    end
+end
